80bd8719313854a8003baa321748750f6d2f3070
LORDDIMAS4IK <Dimaklochihin@yandex.ru>
2023-03-09 16:56:19 +0300

    Решение лабы другим способом

--- thirdlaba.py
+++ thirdlaba.py
@@ -4,4 +4,14 @@
 # Найти сумму цифр целой части и произведение цифр дробной части.
 # Вывести исходное число, сумму и произведение в удобным для чтения формате.
 # Мое число 583.9642
-a = 583.9642
+number = 583.9642
+wholesum = 0
+productfrac = 1
+wholefrac = str(number).split('.')
+for i in wholefrac[0]:
+    wholesum += int(i[0])
+for j in wholefrac[1]:
+    productfrac *= int(j[0])
+print("Исходное число", number)
+print("Сумма цифр целой части", wholesum)
+print("Произведение цифр дробной части числа", productfrac)
