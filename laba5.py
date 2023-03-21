c439401850770e2ba3eb9140ed7758084b20418d
LORDDIMAS4IK <Dimaklochihin@yandex.ru>
2023-03-21 19:05:21 +0300

    пятая лаба

---
+++ laba5.py
@@ -0,0 +1,26 @@
+val_s = 'Dmitriy'
+val_i = 7
+val_f = 8 / 10.
+val_b = 70 < 8
+val_nb = not val_b
+print(val_s, type(val_s))
+print(val_i, type(val_i))
+print(val_f, type(val_f))
+print(val_b, type(val_b))
+print(val_nb, type(val_nb))
+b = val_i + val_f
+print(b,type(b))
+c = val_i*val_f
+print(c,type(c))
+d = val_s * val_i
+print(d,type(d))
+e = val_i/val_f
+print(e,type(e))
+f = val_i//val_f
+print(f,type(f))
+g = val_i%val_f
+print(g,type(g))
+h = val_f//val_i
+print(h,type(h))
+i = val_f%val_i
+print(i,type(i))
