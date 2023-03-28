6819aa41bc536abb0b3c469403c936294010f7d4
LORDDIMAS4IK <Dimaklochihin@yandex.ru>
2023-03-28 18:12:18 +0300

    Лаба 8

--- laba8.py
+++ laba8.py
@@ -5,9 +5,12 @@
 # 2. Функции, определяющие, является ли треугольник равнобедренным и равносторонним.
 # 3. Создать программу, проверяющую треугольник, находящую его равнобедренность и разносторонность в случае успешной проверке, и выводящую результат.
 # Ввод длин сторон (точка входа в программу) функции (положительность(positivity) проверка(check) равнобедренность (isosceles) равностороннесть(equilateral) )
-a = int(input())
-b = int(input())
-c = int(input())
+# Добавить проверку на разносторонность, сделать проверку ненулевой площади!
+import math
+
+a = float(input())
+b = float(input())
+c = float(input())
 
 
 def positivity(a, b, c):
@@ -34,10 +37,20 @@ def isoscele(a, b, c):
 def equilateral(a, b, c):
     if a == b == c:
         return "Triangle is an equilateral"
-    else: return "Triangle is not an equilateral"
+    else:
+        return "Triangle is not an equilateral"
+
+
+def S(a, b, c):
+    p=(a+b+c)/2
+    S = math.sqrt(p*(p-a)*(p-b)*(p-c))
+    # Площадь будем считать по формуле Герона
+    if S > 0: return "S > 0"
+    else: return "S <= 0"
 
 
 print(positivity(a, b, c), "\n",
       check(a, b, c), "\n",
       isoscele(a, b, c), "\n",
-      equilateral(a, b, c))
+      equilateral(a, b, c), "\n",
+      S(a,b,c))
