067e7c48dc85923d2e318d22e0ad0777d13c0fc9
LORDDIMAS4IK <Dimaklochihin@yandex.ru>
2023-04-20 10:08:35 +0300

    Выполнена лабораторная работа

--- laba9.py
+++ laba9.py
@@ -11,7 +11,8 @@
 # Создать функцию, сортирующую список seq по заданной функции сравнения comparator(): [1,2,3,4,5,7,6] - [1,2,3,4,5,6,7]
 # def my_sort(seq, comparator):
 #  # Ваш код
-# Оценить зависимость времени работы от количества элементов массива, например с помощью модуля time. Для более точной оценки времени, сортировку следует проводить несколько раз (n) и определить среднее время:
+# Оценить зависимость времени работы от количества элементов массива, например с помощью модуля time.
+# Для более точной оценки времени, сортировку следует проводить несколько раз (n) и определить среднее время:
 # import time
 # start_time = time.time()
 # n = 10
@@ -26,6 +27,7 @@
 # my_sort(seq, comp)
 # print(seq) # 1, 2, 3
 import random
+import time
 
 s = 701
 random.seed(s)
@@ -35,15 +37,22 @@ seq1000 = [random.random() for _ in range(1000)]
 
 
 def comp(x, y):  # x, y -- float
+    """Функция сравнения"""
     return x < y
 
 
-def my_sort(seq):
-    for i in range(len(seq)):
-        if comp(seq[i-1],seq[i])==True:
-            return seq
-        else:
-            return "Что-то не так"
-        return comp
-    return
-print(my_sort(seq10))
+def my_Sort(z, comparator):
+    """Функция сортировки реализована через сортировку "пузырьком" """
+    for i in range(len(z) - 1):
+        for j in range(len(z) - 2, i - 1, -1):
+            if comparator(z[j + 1], z[j]):
+                z[j], z[j + 1] = z[j + 1], z[j]
+
+    return z
+
+
+start_time: float = time.time()
+n = 10
+for i in range(n):
+    my_Sort(seq1000, comp)
+    print((time.time() - start_time) / n)
