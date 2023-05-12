065acffcce8656f2c686ec9ad8a149b3ce4817b3
LORDDIMAS4IK <klochixindima8@gmail.com>
2023-05-12 22:25:27 +0300

    just do it

--- laba8dop.py
+++ laba8dop.py
@@ -1,58 +1,35 @@
-# Написать программу которая в зависимости от вводимых в программу входных параметров
-# собирает её выполнение из функций. Программа для расчета траектории движения.
-import math
+def move(distance, x, y, direction):
+    if direction == 0:  # вправо
+        x += distance
+    elif direction == 90:  # вверх
+        y += distance
+    elif direction == 180:  # влево
+        x -= distance
+    elif direction == 270:  # вниз
+        y -= distance
 
-import math
+    return x, y
 
-y = 0
-x = 0
+x, y = 0, 0  # начальные координаты черепахи
 
+while True:
+    command = input("Введите команду (вверх, вниз, влево, вправо, выход): ").lower()
+    while command not in ["вверх", "вниз", "влево", "вправо", "выход"]:
+        print("Error")
+        command = input("Введите команду (вверх, вниз, влево, вправо, выход): ").lower()
 
-def Up(a):
-    if a == 'up' or a == 'UP' or a == 'Up':
-        global y
-        y = y + 1
-    else:
-        Exception
+        if command == "выход":
+            break
 
+    distance = int(input("Введите расстояние: "))
+    if command == "вверх":
+        direction = 90
+    elif command == "вниз":
+        direction = 270
+    elif command == "влево":
+        direction = 180
+    elif command == "вправо":
+        direction = 0
 
-def Left(a):
-    if a == 'left' or a == 'LEFT' or a == 'Left':
-        global x
-        x = x - 1
-
-    else:
-        Exception
-
-
-def Right(a):
-    if a == 'right' or a == 'RIGHT' or a == 'Right':
-        global x
-        x = x + 1
-
-    else:
-        Exception
-
-
-def Down(a):
-    if a == 'down' or a == 'DOWN' or a == 'Down':
-        global y
-        y = y - 1
-    else:
-        Exception
-
-
-n = ''
-while n != 'Stop':
-    print("Желаете ли вы остановить черевашку. Если да введите Stop")
-    n = input()
-    print('Если вы хотите чтоб черепашка пошла наверх. Напишите. Налево Left. Направо Right. Вниз Down')
-    a = input()
-
-    Right(a)
-    Down(a)
-    Up(a)
-    Left(a)
-
-    print('Координаты черепашки')
-    print(x, y)
+    x, y = move(distance, x, y, direction)
+    print(f"Позиция черепахи: ({x}, {y})")
